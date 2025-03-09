f78e313c4097b72b35f0e1eb3b478fe78662941c
Yuta Hayashibe <yuta@hayashibe.jp>
2025-03-09 11:27:57 +0900

    Add util

--- init.lua
+++ init.lua
@@ -1,26 +1,5 @@
-local function load_lua_files_from(dir)
-    local luv = vim.loop
-    local path = vim.fn.stdpath("config") .. "/lua/" .. dir
-    local handle = luv.fs_scandir(path)
-
-    if not handle then
-        return
-    end
-
-    while true do
-        local name = luv.fs_scandir_next(handle)
-        if not name then
-            break
-        end
-
-        if name:match("%.lua$") then
-            local module_name = dir .. "." .. name:gsub("%.lua$", "")
-            require(module_name)
-        end
-    end
-end
-
 require("general")
+require("config.util")
 
 require("config.lazy")
 
