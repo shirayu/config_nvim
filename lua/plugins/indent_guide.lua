2a4d3690a2537f798967d7ec0762064f8d51174e
Yuta Hayashibe <yuta@hayashibe.jp>
2025-03-09 23:17:32 +0900

    Add indent_guide

---
+++ lua/plugins/indent_guide.lua
@@ -0,0 +1,54 @@
+return
+{
+    "shellRaining/hlchunk.nvim",
+    lazy = true,
+    opts = {
+        indent = {
+            enable = true,
+            priority = 10,
+            -- style = { "#888888" },
+            use_treesitter = false,
+            chars = {
+                "¦",
+                "┆",
+                "┊",
+            },
+            -- ahead_lines = 20,
+            delay = 100
+        },
+        chunk = {
+            enable = false,
+        },
+        blank = {
+            enabled = false,
+            priority = 5
+        },
+        line_num = {
+            enable = false,
+            priority = 4
+        }
+    },
+    keys = {
+        {
+            mode = "n",
+            "<C-i>",
+            function()
+                if vim.g.hlchunkDisabled then
+                    vim.cmd('EnableHLIndent')
+                else
+                    vim.cmd('DisableHLIndent')
+                end
+                vim.g.hlchunkDisabled = not vim.g.hlchunkDisabled
+            end,
+            desc = "DisableHLIndent",
+            noremap = true,
+            silent = true,
+        },
+    },
+
+    config = function(_, opts)
+        require('hlchunk').setup(opts)
+        vim.g.hlchunkDisabled = true
+    end,
+
+}
