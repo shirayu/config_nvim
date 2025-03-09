652d7b8f88e72fef8698dd5ef50345a6b2fefcd9
Yuta Hayashibe <yuta@hayashibe.jp>
2025-03-09 18:13:14 +0900

    Moved lazy-lock.json

--- init.lua
+++ init.lua
@@ -3,8 +3,12 @@ require("config.util")
 
 require("config.lazy")
 
-require("lazy").setup({
+local plugins = {
     { import = "plugins" },
-})
+}
+local opts = {
+    lockfile = vim.fn.stdpath("config") .. "/lock/lazy-lock.json",
+}
+require("lazy").setup(plugins, opts)
 
 load_lua_files_from("lang")
