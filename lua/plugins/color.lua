7a7c90f476d743d5c19c76add0925e0ca1c00231
Yuta Hayashibe <yuta@hayashibe.jp>
2025-03-10 20:18:54 +0900

    Added rainbow_csv

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -8,4 +8,16 @@ return {
             },
         },
     },
+    {
+        'mechatroner/rainbow_csv',
+        ft = {
+            'csv',
+            'tsv',
+            'csv_semicolon',
+            'csv_whitespace',
+            'csv_pipe',
+            'rfc_csv',
+            'rfc_semicolon'
+        },
+    }
 }
