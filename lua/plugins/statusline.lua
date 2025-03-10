5d070035bb6cf8b683e12f252fcce5118ce0cf42
Yuta Hayashibe <yuta@hayashibe.jp>
2025-03-10 10:59:34 +0900

    Fix

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -2,7 +2,7 @@ return {
     "nvim-lualine/lualine.nvim",
     config = function()
         require("lualine").setup({
-            options = { theme = "ayu_dark" },
+            options = { theme = "onedark" },
             sections = {
                 lualine_c = {
                     {
