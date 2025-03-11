6e5709e8bcfb1668d10aee810a2fb5d7f2ef52ed
Yuta Hayashibe <yuta@hayashibe.jp>
2025-03-11 10:50:44 +0900

    Add

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -7,5 +7,10 @@ return {
   },
   config = function()
     vim.cmd.colorscheme("onedark")
+
+    vim.api.nvim_set_hl(0,         "SpellBad",   { bg = "#fce3fc", underline = true })
+    -- vim.api.nvim_set_hl(0,         "SpellCap",   { bg = "#fce3fc", underline = true })
+    -- vim.api.nvim_set_hl(0,         "SpellLocal", { bg = "#fce3fc", underline = true })
+    -- vim.api.nvim_set_hl(0,         "SpellRare",  { bg = "#fce3fc", underline = true })
   end
 }
