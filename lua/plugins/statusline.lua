6d9e08d74844eaed72cc7735f401f028b900bc73
Yuta Hayashibe <yuta@hayashibe.jp>
2025-03-18 20:54:33 +0900

    Show Unicode positions

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,3 +1,16 @@
+local function unicode_at_cursor()
+  local pos = vim.api.nvim_win_get_cursor(0)
+  local row = pos[1]
+  local col = pos[2]
+  local line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1]
+  if not line or col >= #line then
+    return ""
+  end
+  local char = line:sub(col + 1, col + 1)
+  return string.format("[%s][U+%04X]", char, vim.fn.char2nr(char))
+end
+
+
 return {
   "nvim-lualine/lualine.nvim",
   config = function()
@@ -16,6 +29,9 @@ return {
             file_status = false, -- displays file status (readonly status, modified status)
             path = 2,            -- 0 = just filename, 1 = relative path, 2 = absolute path
           },
+        },
+        lualine_z = {
+          { "location" }, { unicode_at_cursor },
         },
       },
     })
