4bbf50c49ec9e54c006f92603ac3413886b7e295
Yuta Hayashibe <yuta@hayashibe.jp>
2025-03-31 02:37:37 +0900

    Fix

--- markdownfix.sh
+++ markdownfix.sh
@@ -23,15 +23,20 @@ trap 'rc=$?; trap - EXIT; atexit; exit $?' INT PIPE TERM
 tmpfile=$(mktemp "/tmp/${0##*/}.tmp.XXXXXX")
 
 CONFIG_ARG="${HOME}/.markdownlintrc"
-path_target="$1"
+path_target=$(dirname "$1")
 while [ "$path_target" != "/" ]; do
-    if [ -f "${path_target}/markdownlint.json" ]; then
-        CONFIG_ARG="${path_target}/markdownlint.json"
+    if [ -f "${path_target}/.markdownlint.json" ]; then
+        CONFIG_ARG="${path_target}/.markdownlint.json"
         break
     fi
     path_target=$(dirname "$path_target")
 done
 
+DIR_MAKDOWNLINT_PARENT=${SCRIPT_DIR}/tools
+if [ -e "${path_target}/node_modules/markdownlint-cli" ]; then
+    DIR_MAKDOWNLINT_PARENT=${path_target}
+fi
+
 cat - >"$tmpfile"
-eval "npx -C ${SCRIPT_DIR}/tools markdownlint -f ${tmpfile} ${CONFIG_ARG}" 2>/dev/null || :
+eval "npx -C ${DIR_MAKDOWNLINT_PARENT} markdownlint -f ${tmpfile} ${CONFIG_ARG}" 2>/dev/null || :
 cat "$tmpfile"
