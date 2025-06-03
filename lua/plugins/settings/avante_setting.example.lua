1127302068d01a1cab067634af5beed14eae6b65
Yuta Hayashibe <yuta@hayashibe.jp>
2025-06-03 14:03:32 +0900

    Fix

--- lua/plugins/settings/avante_setting.example.lua
+++ lua/plugins/settings/avante_setting.example.lua
@@ -5,17 +5,9 @@
 -- vim.fn.setenv("OPENAI_API_KEY", "DUMMY")
 
 return {
-  provider = "ollama",
-  ollama = {
-    model = "qwq:32b",
+  providers = {
+    ollama = {
+      model = "qwq:32b",
+    }
   },
-
-  --   openai = {
-  --     endpoint = "https://api.openai.com/v1",
-  --     model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-  --     timeout = 30000,  -- timeout in milliseconds
-  --     temperature = 0,  -- adjust if needed
-  --     max_tokens = 4096,
-  --   },
-
 }
